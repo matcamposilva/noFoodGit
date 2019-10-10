5e23d4f6445cf754676d10d77dc60ff7bf7f5c60
matcamposilva <matcamposilva@gmail.com>
2019-10-09 21:18:27 -0300

    adicionando  controller e routes

---
+++ NoFood.Api/bin/express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//routers
+const categoriaRouter = require('../routes/catergoria-router');
+const produtoRouter = require('../routes/produto-router');
+
+//Criando/Invocando a Api/Server Web do Express
+const app = express();
+
+//Configuração de parse do JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+//Configurando as rotas
+app.use('/api/categoria', categoriaRouter);
+app.use('/api/produto', produtoRouter);
+
+
+//Exportando nossa Api
+module.exports = app;
