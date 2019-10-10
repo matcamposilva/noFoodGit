df5d617a5d0683b7fe5b36db2883571c58dba0b9
matcamposilva <matcamposilva@gmail.com>
2019-10-09 22:19:33 -0300

    add mongoDB 1

--- NoFood.Api/bin/configuration/variables.js
+++ NoFood.Api/bin/configuration/variables.js
@@ -1,6 +1,9 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database: {
+        connection: process.env.connection || ("mongodb+srv://admin>:<noFood123456>@cluster0-v3fui.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true })
     }
 }
 module.exports = variables;
--- NoFood.Api/server.js
+++ NoFood.Api/server.js
@@ -1,55 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+'use strict'
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = require('../NoFood.Api/bin/express');
+const variables = require('../NoFood.Api/bin/configuration/variables');
 
-//CRUD
-
-//Verbs http (Verbos Http)
-
-let pessoas = [];
-
-//R - READ
-app.get('/', (req, res) => {
-    res.status(200).send(pessoas);
+app.listen(variables.Api.port, () => {
+    console.info(`Api inicializada com sucesso na porta ${variables.Api.port}`);
 });
-
-//C - CREATE
-app.post('/', (requisicao, resposta) => {
-    console.log('Corpo:', requisicao.body);
-    pessoas.push(requisicao.body);
-    resposta.status(201).send(requisicao.body);
-});
-
-//U - UPDATE
-app.put('/:id', (req, res) => {
-    let pessoaEncontrada = pessoas.filter(pes => { return pes.id == req.params.id });
-    pessoaEncontrada = req.body;
-    res.status(202).send(pessoaEncontrada);
-});
-
-//D - DELETE
-app.delete('/:id', (req, res) => {
-    for (let index = 0; index < pessoas.length; index++) {
-        const pessoa = pessoas[index];
-        if (pessoa.id == req.params.id) {
-            pessoas.splice(index, 1);
-        }
-    }
-    res.status(204).send();
-});
-
-
-//400 - bad request
-//401 - Unauthorized
-//500 - internal server error
-
-
-
-app.listen(3000, () => {
-    console.log('Servidor Api NoFood iniciado na porta 3000.');
-});
-
-
